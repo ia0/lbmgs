645ca13d422c5538cc9b7dee083ca5530dcd85c6
ia0 <git@ia0.eu>
2014-08-16 20:27:56 +0200

    client/server splitting at main

--- src/chess.c
+++ src/chess.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void __attribute__((noreturn))
+usage(int status)
+{
+	fputs("\
+usage: chess { server | client } <host> <port>\n\
+", stdout);
+	exit(status);
+}
+
+void __attribute__((noreturn))
+server(char *host, char *port)
+{
+	printf("server %s %s\n", host, port);
+	exit(EXIT_SUCCESS);
+}
+
+void __attribute__((noreturn))
+client(char *host, char *port)
+{
+	printf("client %s %s\n", host, port);
+	exit(EXIT_SUCCESS);
+}
 
 int
-main(void)
+main(int argc, char *argv[])
 {
-	printf("Hello world!\n");
-	exit(EXIT_SUCCESS);
+	if (argc == 2 && !strcmp("--help", argv[1]))
+		usage(EXIT_SUCCESS);
+	if (argc != 4)
+		usage(EXIT_FAILURE);
+	if (!strcmp("server", argv[1]))
+		server(argv[2], argv[3]);
+	if (!strcmp("client", argv[1]))
+		client(argv[2], argv[3]);
+	usage(EXIT_FAILURE);
 }
